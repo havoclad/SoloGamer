9e434bbc7cf7cdf84a09b0077895cc39ccae32d4
Pat Ludwig <havoc@boldo.com>
2017-09-02 00:13:45 -0500

    Initial savefile
    - Added .gitignore for build files
    - removed build files that snuck in!
    - Code for save file
    - run.bash add -v flag to mount save directory external to container

--- perl/b17.pl
+++ perl/b17.pl
@@ -3,6 +3,7 @@
 use strict;
 
 use JSON::Parse qw/json_file_to_perl parse_json/;
+use JSON::Tiny qw/ encode_json /;
 
 my $p = json_file_to_perl('/data/G-1');
 
@@ -11,4 +12,8 @@ my $r = int(rand(6)) +1;
 my $dest = $p->{$r}->{'Target'};
 
 print "We're going to $dest\n";
+
+open (SAVE, ">",  "/save/pat") or die $!;
+print SAVE encode_json($p);
+close SAVE;
 
--- run.bash
+++ run.bash
@@ -1,3 +1,3 @@
 #!/usr/local/bin/bash
 
-docker run --rm -it havoclad/b17
+docker run -v /Users/pludwig/testing/save:/save --rm -it havoclad/b17
