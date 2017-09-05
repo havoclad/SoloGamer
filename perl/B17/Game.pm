0681a29024df1e5a17c72a4b25f5e7edd57843c2
Pat Ludwig <havoc@boldo.com>
2017-09-04 23:35:53 -0500

    Loadtables in the game package now
    - Fix spacing issue in G-1
    - Remove save game cludge

--- perl/B17/Game.pm
+++ perl/B17/Game.pm
@@ -2,9 +2,12 @@ package B17::Game;
 
 use strict;
 
+use File::Basename;
+
 use Moose;
 use namespace::autoclean;
 
+use B17::LoadTable;
 
 has 'name' => (
   is       => 'ro',
@@ -29,6 +32,14 @@ has 'source' => (
   builder  => '_build_source',
 );
 
+has 'table' => (
+  is       => 'ro',
+  isa      => 'HashRef',
+  lazy     => 1,
+  required => 1,
+  builder  => '_load_data_tables',
+);
+
 sub _build_source {
   my $self = shift;
   return '/games/' . $self->name . '/';
@@ -39,5 +50,15 @@ sub _build_source_data {
   return $self->source . 'data/';
 }
 
+sub _load_data_tables {
+  my $self = shift;
+
+  my $h = {};
+  my $dir = $self->source_data;
+  foreach my $table (<$dir/*>) {
+    $h->{basename $table} = new B17::LoadTable( file => $table );
+  }
+  return $h;
+}
 
 __PACKAGE__->meta->make_immutable;
--- perl/b17.pl
+++ perl/b17.pl
@@ -1,29 +1,19 @@
 #!/usr/local/bin/perl
 
 use strict;
+use warnings;
 
 use Mojo::JSON qw(decode_json encode_json);
 
 use lib '/perl';
 
 use B17::Game;
-use B17::LoadTable;
 
 my $game = new B17::Game(
   name => $ENV{'GAME'},
 );
 
-my $s = $game->source_data;
-my $p = new B17::LoadTable( file => $s . 'G-1');
+my $r = $game->table->{'G-1'}->roll;
 
-my $r = $p->roll;
-
-my $dest = $r->{'Target'};
-my $type = $r->{'Type'};
-
-print "Our target is the $type in $dest\n";
-
-open (SAVE, ">",  "/save/pat") or die $!;
-print SAVE encode_json($p);
-close SAVE;
+printf "Our target is the %s in %s.\n", $r->{Type}, $r->{Target};
 
